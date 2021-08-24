7cf4270c44932d9a62742d6da6cc5e27dc6b20bb
Michael Brown <michael@excentrix.net>
2021-08-24 14:03:01 -0400

    Forgot to delete a file :)

---
+++ cmake-build-debug/CMakeFiles/Homework_CS232.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homework_CS232.dir/hw1/helloCS232.c.o"
+  "Homework_CS232"
+  "Homework_CS232.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Homework_CS232.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
